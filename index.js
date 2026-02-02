2ed14028914ff54332fefc53636cdba7cc64b50e
Princewill Monday <princewillmonday1999@gmail.com>
2026-02-02 12:51:53 +0100

    variable test

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-let CarName="Toyota" // Pascal Case
-let carName ="Corolla" // Camel Case
-let car_name= "Honda" // Snake Case
-let carname= "Lexus" // Kebab
+// let CarName="Toyota" // Pascal Case
+// let carName ="Corolla" // Camel Case
+// let car_name= "Honda" // Snake Case
+// let carname= "Lexus" // Kebab
+
+let y = 5
+let x = --y
+console.log(x)
