5d68bc6185df7d3f4301e895362aa6cddcff5602
Princewill Monday <princewillmonday1999@gmail.com>
2026-02-06 13:29:31 +0100

    Working on JavaScript for Internal and External

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
-snack = "Moi Moi"
+// snack = "Moi Moi"
 
-if (snack == "Burger") {
-    console.log (`We have ${snack}`)
-} else if (snack == "Egg Row") {
-    console.log (`We have ${snack}`)
-} else if (snack == "Meat Pi") {
-    console.log (`We have ${snack}`)
-} else if (snack == "Moi Moi") {
-    console.log (`We have ${snack}`)
-}else {
-    console.log (`We do not have ${snack}`)
+// if (snack == "Burger") {
+//     console.log (`We have ${snack}`)
+// } else if (snack == "Egg Row") {
+//     console.log (`We have ${snack}`)
+// } else if (snack == "Meat Pi") {
+//     console.log (`We have ${snack}`)
+// } else if (snack == "Moi Moi") {
+//     console.log (`We have ${snack}`)
+// }else {
+//     console.log (`We do not have ${snack}`)
+// }
+
+// let movieAge = 40
+
+// if (movieAge<18) {
+//     console.log(`You cannot watch this movie, you are ${movieAge} years old and you need parental control`)
+// } else if (movieAge==18){
+//     console.log(`You are ${movieAge} years old and you have access to some certain movies`)
+// }
+// else {
+//     console.log(`You are ${movieAge} years old and you can watch any movie of your choice`)
+// }
+
+// let courseAge = 6
+
+// if (courseAge<=4) {
+//     console.log(`You are ${courseAge} yeard old and you are too young to learn coding`)
+// }else if (courseAge==5 || courseAge<=8) {
+//     console.log(`You are ${courseAge} years old and you can now learn Scratch Programming`)
+// }
+
+let num = 5
+
+if (num%2 == 0) {
+    console.log(`This is an even number. ${num}`)
+}else{
+    console.log(`This is an odd number. ${num}`)
 }
